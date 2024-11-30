105db39bc14fe2058e4daa7275210bf9f2ed3c98
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-11-30 22:53:44 +0100

    first commit

---
+++ 01b.cc
@@ -0,0 +1,23 @@
+#include <cassert>
+#include <cstdint>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
+int main() {
+
+  for (std::string line; std::getline(std::cin, line); ) {
+    if (line.empty() || line == "\n") {
+      continue;
+    }
+  }
+
+}
---
+++ xmake.lua
@@ -0,0 +1,224 @@
+set_project("adventofcode")
+set_version("2024")
+
+add_requires("boost")
+
+set_policy("build.warning", true)
+set_symbols("debug")
+set_warnings("allextra")
+set_languages("cxx17")
+set_encodings("utf-8")
+
+target("01a")
+    set_kind("binary")
+    add_files("01a.cc")
+
+target("01b")
+    set_kind("binary")
+    add_files("01b.cc")
+
+-- target("02a")
+--     set_kind("binary")
+--     add_files("02a.cc")
+--
+-- target("02b")
+--     set_kind("binary")
+--     add_files("02b.cc")
+--
+-- target("03a")
+--     set_kind("binary")
+--     add_files("03a.cc")
+--
+-- target("03b")
+--     set_kind("binary")
+--     add_files("03b.cc")
+--
+-- target("04a")
+--     set_kind("binary")
+--     add_files("04a.cc")
+--
+-- target("04b")
+--     set_kind("binary")
+--     add_files("04b.cc")
+--
+-- target("05a")
+--     set_kind("binary")
+--     add_files("05a.cc")
+--
+-- target("05b")
+--     set_kind("binary")
+--     add_files("05b.cc")
+--
+-- target("06a")
+--     set_kind("binary")
+--     add_files("06a.cc")
+--
+-- target("06b")
+--     set_kind("binary")
+--     add_files("06b.cc")
+--
+-- target("07a")
+--     set_kind("binary")
+--     add_files("07a.cc")
+--
+-- target("07b")
+--     set_kind("binary")
+--     add_files("07b.cc")
+--
+-- target("08a")
+--     set_kind("binary")
+--     add_files("08a.cc")
+--
+-- target("08b")
+--     set_kind("binary")
+--     add_files("08b.cc")
+--
+-- target("09a")
+--     set_kind("binary")
+--     add_files("09a.cc")
+--
+-- target("09b")
+--     set_kind("binary")
+--     add_files("09b.cc")
+--
+-- target("10a")
+--     set_kind("binary")
+--     add_files("10a.cc")
+--
+-- target("10b")
+--     set_kind("binary")
+--     add_files("10b.cc")
+--
+-- target("10display")
+--     set_kind("binary")
+--     add_files("10display.cc")
+--
+-- target("11a")
+--     set_kind("binary")
+--     add_files("11a.cc")
+--
+-- target("11b")
+--     set_kind("binary")
+--     add_files("11b.cc")
+--
+-- target("12a")
+--     set_kind("binary")
+--     add_files("12a.cc")
+--
+-- target("12b")
+--     set_kind("binary")
+--     add_files("12b.cc")
+--
+-- target("13a")
+--     set_kind("binary")
+--     add_files("13a.cc")
+--
+-- target("13b")
+--     set_kind("binary")
+--     add_files("13b.cc")
+--
+-- target("14a")
+--     set_kind("binary")
+--     add_files("14a.cc")
+--
+-- target("14b")
+--     set_kind("binary")
+--     add_files("14b.cc")
+--
+-- target("15a")
+--     set_kind("binary")
+--     add_files("15a.cc")
+--
+-- target("15b")
+--     set_kind("binary")
+--     add_files("15b.cc")
+--
+-- target("16a")
+--     set_kind("binary")
+--     add_files("16a.cc")
+--
+-- target("16b")
+--     set_kind("binary")
+--     add_files("16b.cc")
+--
+-- target("17a")
+--     set_kind("binary")
+--     add_files("17a.cc")
+--     add_packages("boost")
+--
+-- target("17b")
+--     set_kind("binary")
+--     add_files("17b.cc")
+--     add_packages("boost")
+--
+-- target("18a")
+--     set_kind("binary")
+--     add_files("18a.cc")
+--     add_packages("boost")
+--
+-- target("18b")
+--     set_kind("binary")
+--     add_files("18b.cc")
+--     add_packages("boost")
+--
+-- target("19a")
+--     set_kind("binary")
+--     add_files("19a.cc")
+--
+-- target("19b")
+--     set_kind("binary")
+--     add_files("19b.cc")
+--
+-- target("20a")
+--     set_kind("binary")
+--     add_files("20a.cc")
+--
+-- target("20b")
+--     set_kind("binary")
+--     add_files("20b.cc")
+--
+-- target("20c")
+--     set_kind("binary")
+--     add_files("20c.cc")
+--
+-- target("21a")
+--     set_kind("binary")
+--     add_files("21a.cc")
+--
+-- target("21b")
+--     set_kind("binary")
+--     add_files("21b.cc")
+--
+-- target("22a")
+--     set_kind("binary")
+--     add_files("22a.cc")
+--
+-- target("22b")
+--     set_kind("binary")
+--     add_files("22b.cc")
+--
+-- target("23a")
+--     set_kind("binary")
+--     add_files("23a.cc")
+--
+-- target("23b")
+--     set_kind("binary")
+--     add_files("23b.cc")
+--
+-- target("24a")
+--     set_kind("binary")
+--     add_files("24a.cc")
+--
+-- target("24b")
+--     set_kind("binary")
+--     add_files("24b.cc")
+--
+-- target("25a")
+--     set_kind("binary")
+--     add_files("25a.cc")
+--     add_packages("boost")
+--
+-- target("25b")
+--     set_kind("binary")
+--     add_files("25b.cc")
+--     add_packages("boost")
